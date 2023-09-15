622a3789cdceabeb908ee0debe512af6c4a39435
Daud Shah <sdaud4214@gmail.com>
2023-09-15 05:01:25 -0600

    my first program 01

---
+++ aa.py
@@ -0,0 +1,5 @@
+import calendar
+yy = 2023
+m9 = 9
+m10 = 10
+print(calendar.month(yy,m9,m10))
---
+++ first program 01.py
@@ -0,0 +1,6 @@
+# Data : 15/9/2023
+# Aoa! Hi this is Daud shah i start learning python today for ML.
+print("Hello world !!!")
+print(" This is my first progarm in pythan ")
+#>>>Today i learned in pythan how to use "print" 
+#>>> wright my first program "Hello world "
