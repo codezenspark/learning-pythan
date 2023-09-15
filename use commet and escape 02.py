8cd8e9e5a1607af1e0b61bbca02a31ec9c1125f3
Daud Shah <sdaud4214@gmail.com>
2023-09-15 05:29:58 -0600

    use commet and escape 02.py

---
+++ use commet and escape 02.py
@@ -0,0 +1,10 @@
+#>>> in this program i learning commet and escape in pythan 
+a =2#this is num 1 
+b=4#this is num 2
+sum=a+b #sum of num1 and num2
+print(sum)#print sum
+#in green writhing is commet.
+# >>> i use escape 
+print("HI i am learning \"pythan\" \nbut i am very \'happy\' to \nlearning pythan ")
+#this is escape charater "\n" is go to next line
+#print("me daud ",456,345,2325,345, sep="@")
