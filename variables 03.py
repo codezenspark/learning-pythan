b3e1474e3d75351b5f5e4129604fef8da4a0f3e6
Daud Shah <sdaud4214@gmail.com>
2023-09-16 05:07:54 -0600

    variable 03

---
+++ variables 03.py
@@ -0,0 +1,9 @@
+#>>> in this program i use variable in pythan
+c = 5
+a = True
+name = "Daud"
+b = 3.45
+print("The data types is " ,type(c))
+print("The data types is " ,type(a))
+print("The data types is " ,type(name))
+print("The data types is " ,type(b))
