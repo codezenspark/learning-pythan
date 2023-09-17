f37613db600ae2bb87bafdd91d535d253ecb40f9
Daud Shah <sdaud4214@gmail.com>
2023-09-17 04:49:23 -0600

    operaters 04

---
+++ operaters 04.py
@@ -0,0 +1,12 @@
+#>>> in this program i use operaters (+,-,*,/,%,//,**) all these operaters use 
+#>>> use Addition two numbers 
+print("Add = ",9+11)
+#>>> use sub two number
+print("sub = ",45-20)
+#>>> use multiplation two number 
+print("mul = ",34*2)
+#>>> use division 
+print("div = ",23/4)
+#>>> use power of number
+print(" 5 power 6 = ",5**6)
+print(34//4)
