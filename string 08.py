4b20c545dd7f7c16f66b3d1f17b49d58cc4f01cd
Daud Shah <sdaud4214@gmail.com>
2023-09-18 05:43:32 -0600

    string 08

---
+++ string 08.py
@@ -0,0 +1,17 @@
+# >>> in this program i use string 
+name = "DAUD"
+lastname = "SHAH"
+print(name,lastname)
+# use for string ("") & ('') for single line string 
+# for multiple line string is use this(""" """) & (''' ''')
+a ="""Hi Daud shah 
+how are you 
+hello ali 
+i am good 
+and you """ 
+print(a)
+# >>> string use for index 01234.....
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
