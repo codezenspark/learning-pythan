2d48b5abd908ef4920f24fb80704f2499e5e06ab
Daud Shah <sdaud4214@gmail.com>
2023-09-18 06:05:31 -0600

    string slicing and length of string 09

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+print(name[0:5])#0 index is inlude but 5 index is not 
+# print(name[0:11])#0 index is inlude but 11 index is not 
+# print(name[6:11])#6 index is inlude but 11 index is not 
+# print(name[4:11])#4 index is inlude but 11 index is not 
