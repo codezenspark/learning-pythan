fa8f9e9a22e43a7452b04a7379da51e521d03c29
Daud Shah <sdaud4214@gmail.com>
2023-09-23 00:26:58 -0600

    if else 10

---
+++ calculter using if else 11.py
@@ -0,0 +1,13 @@
+a = int(input(print("Enter the first number")))
+o = input(print("+,-,*,/"))
+b = int(input(print("Enter the second number")))
+if(o == '+'):
+    print("a+b = ",a+b)
+elif(o == '-'):
+    print("a-b = ",a-b)
+elif(o == '*'):
+    print("a*b = ",a*b)
+elif(o == '/'):
+    print("a/b = ",a/b)
+else:
+    "non"
---
+++ if else 10.py
@@ -0,0 +1,21 @@
+# >>> in this program i use if else statment , elif and nasted if else
+# a = int(input(print("enter your age")))
+
+# if(a>18):
+#     print("welcome to culb")
+# else:
+#     print("sorry!! you are not allow")
+
+#>>>> elif
+b = 107
+if(b>1000):
+    print("b is greater than 100")
+elif(b>100):
+    print("B IS greater than 100")
+    if(b%2 != 0):
+        print("b is odd ")
+    else:
+        print("b is even")
+else:
+    print("b is big")    
+  
