58db1200328a4afbcacdd823a08a79ff806c4849
Daud Shah <sdaud4214@gmail.com>
2023-09-23 00:27:59 -0600

    calculter use if else 11

--- login using if else 12.py
+++
@@ -1,7 +0,0 @@
-
-a = input(print("Enter the user name "))
-b = int(input(print("Enter password")))
-if(a == 'daudshah', b == 1234):
- print("welcome")
-else:
- print("your name or password is worng")
